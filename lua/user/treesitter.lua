30aa846775a73f7f446ce483e691fd4b8fdc8490
crisnLopezMeli <cristian.lopez@mercadolibre.com>
2022-08-30 23:10:37 -0300

    fix | telescope find files highlighter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -2,14 +2,28 @@ local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then return end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
+	ensure_installed = {
+    "dockerfile",
+    "go",
+    "gomod",
+    "json",
+    "lua",
+    "make",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "regex",
+    "sql",
+    "vim",
+    "yaml",
+  }, -- one of "all" or a list of languages
+	ignore_install = {}, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
+		disable = { }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { "python", "css" } },
+	indent = { enable = false , disable = { "python", "css" } },
 })
